0ee3b928150f5c0777c9f8b4731754461e50c9c0
Joachim Metz <joachim.metz@gmail.com>
2020-02-16 14:41:07 +0100

    Added libfsapfs test scripts

---
+++ libfsapfs/run-linux.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#
+# Script to create a bodyfile and hash file content.
+
+fsapfstools/fsapfsinfo -B ${BODYFILE} -H -f 1 -o ${OFFSET} -r ${RECOVERY_PASSWORD} ${IMAGE}
+
+mkdir -p fuse
+
+fsapfstools/fsapfsmount -f 1 -o ${OFFSET} -r ${RECOVERY_PASSWORD} ${IMAGE} fuse
+
+(cd fuse/ && find . -type f -exec md5sum {} \; > ${HOME}/MD5SUMS )
+
+cat ${HOME}/MD5SUMS | sort -k2 > ${HOME}/MD5SUMS.sorted
+
+# Remove sockets
+grep -v d41d8cd98f00b204e9800998ecf8427e ${HOME}/MD5SUMS.sorted > ${HOME}/MD5SUMS.filtered
+
---
+++ libfsapfs/run-macos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Script to create a bodyfile and hash file content on Mac OS.
+
+sudo hdiutil attach -imagekey diskimage-class=CRawDiskImage -blocksize ${SECTORSIZE} -nomount -readonly ${IMAGE}
+
+diskutil apfs unlockVolume ${DEVICE}
+
+(cd ${MOUNT_POINT} && find . -exec stat -f "0|%N|%i|%p|%u|%u|%z|%a.0|%m.0|%c.0|%B.0" -t "%s" {} \; > ${HOME}/bodyfile );
+(cd ${MOUNT_POINT} && find . -type f -exec md5 {} \; > ${HOME}/MD5SUMS );
+
+cat ${HOME}/MD5SUMS | sed 's/^MD5 (\(.*\)) = \(\S*\)/\2  \1/' | sort -k2 > ${HOME}/MD5SUMS.sorted
+
+sudo hdiutil detach ${DEVICE}
+
