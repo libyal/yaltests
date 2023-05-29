14e112abf70409cb9cfff7c0a8f614dec955612c
Joachim Metz <joachim.metz@gmail.com>
2023-05-29 18:22:49 +0200

    Worked on dtFormats scripts

--- dtformats/run_test_unified_logging.sh
+++ dtformats/run_test_unified_logging.sh
@@ -18,18 +18,18 @@ set -e
 
 echo "Scanning image for .tracev3 files";
 
-# TODO: iterate all partitions individually or use dfImageTools partition identifier?
-
-TRACEV3_FILES=`PYTHONPATH=${DFIMAGETOOLS} ${DFIMAGETOOLS}/tools/list_file_entries.py --partitions 2 $1 | grep ".tracev3" | sed 's/^[^|]*|//;s/|.*//'`;
+TRACEV3_FILES=`PYTHONPATH=${DFIMAGETOOLS} ${DFIMAGETOOLS}/tools/list_file_entries.py --no-aliases --partitions=all --snapshots=none --volumes=all $1 | grep ".tracev3" | sed 's/^[^|]*|//;s/|.*//'`;
 
 for TRACEV3_FILE in ${TRACEV3_FILES};
 do
-	# Strip the partition and volume identifiers.
-	# TODO: extend this.
-	TRACEV3_FILE=`echo ${TRACEV3_FILE} | sed 's?^/gpt[^/]*/?/?'`;
+	PARTITIONS_ARGUMENT=`echo ${TRACEV3_FILE} | grep -e '^/p[0-9][0-9]*/' | sed 's?^/p\([0-9][0-9]*\)/.*?--partitions=\1?'`;
+	TRACEV3_FILE=`echo ${TRACEV3_FILE} | sed 's?^/p[0-9][0-9]*/?/?'`;
+
+	VOLUMES_ARGUMENT=`echo ${TRACEV3_FILE} | grep -e '/^apfs[0-9][0-9]*' | sed 's?^/apfs\([0-9][0-9]*\)/.*?--volumes=\1?'`;
+	TRACEV3_FILE=`echo ${TRACEV3_FILE} | sed 's?^/apfs[0-9][0-9]*/?/?'`;
 
 	echo "Processing: ${TRACEV3_FILE}";
-	PYTHONPATH=${DTFORMATS} ${DTFORMATS}/scripts/unified_logging.py --image $1 --partitions 2 ${TRACEV3_FILE} > /dev/null;
+	PYTHONPATH=${DTFORMATS} ${DTFORMATS}/scripts/unified_logging.py --image=$1 ${PARTITIONS_ARGUMENT} ${VOLUMES_ARGUMENT} ${TRACEV3_FILE} > /dev/null;
 done
 
 echo "Processing completed";
